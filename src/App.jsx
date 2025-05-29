27588376d1d83d02f47c249a988c4ed79d1e25f8
APerlinx <133282938+APerlinx@users.noreply.github.com>
2025-05-29 23:15:22 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+import GlobalStyles from '../src/styles/GlobalStyles'
+import Button from './ui/Button'
+import Input from './ui/Input'
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  text-align: center;
+`
+
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
+`
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert('Checked in')}>Check in</Button>
+        <Button onClick={() => alert('Checked out')}>Check out</Button>
+        <Input type="number" placeholder="Number of guests" />
+      </StyledApp>
+    </>
+  )
+}
+
+export default App
