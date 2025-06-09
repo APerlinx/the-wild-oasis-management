5642d4e278b6a5fe0a1e8c326b02e6a69da63372
APerlinx <133282938+APerlinx@users.noreply.github.com>
2025-06-09 15:22:43 +0300

    Connect supabase and intial data, Add React query

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,5 +1,10 @@
-import styled from "styled-components";
-
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+import { useQuery } from '@tanstack/react-query'
+import styled from 'styled-components'
+import { getCabins } from '../../services/apiCabins'
+import Spinner from '../../ui/Spinner'
+import CabinRow from './CabinRow'
 const Table = styled.div`
   border: 1px solid var(--color-grey-200);
 
@@ -7,7 +12,7 @@ const Table = styled.div`
   background-color: var(--color-grey-0);
   border-radius: 7px;
   overflow: hidden;
-`;
+`
 
 const TableHeader = styled.header`
   display: grid;
@@ -22,4 +27,33 @@ const TableHeader = styled.header`
   font-weight: 600;
   color: var(--color-grey-600);
   padding: 1.6rem 2.4rem;
-`;
+`
+
+function CabinTable() {
+  const {
+    isLoading,
+    data: cabins,
+    error,
+  } = useQuery({
+    queryKey: ['cabin'],
+    queryFn: getCabins,
+  })
+  if (isLoading) return <Spinner />
+  return (
+    <Table role="table">
+      <TableHeader role="row">
+        <div></div>
+        <div>Cabin</div>
+        <div>Capacity</div>
+        <div>Price</div>
+        <div>Discount</div>
+        <div></div>
+      </TableHeader>
+      {cabins.map((cabin) => (
+        <CabinRow cabin={cabin} key={cabins.id} />
+      ))}
+    </Table>
+  )
+}
+
+export default CabinTable
