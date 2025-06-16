c3f44cf525904913e8ebf8350f03821deb1614a1
APerlinx <133282938+APerlinx@users.noreply.github.com>
2025-06-16 23:40:43 +0300

    Add Sorting to cabins

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -14,13 +14,19 @@ function CabinTable() {
   if (isLoading) return <Spinner />
 
   const filterValue = searchParams.get('discount') || 'all'
-
   const filteredCabins =
     filterValue === 'all'
       ? cabins
       : filterValue === 'with-discount'
       ? cabins?.filter((cabin) => cabin.discount)
       : cabins?.filter((cabin) => !cabin.discount)
+
+  const sortBy = searchParams.get('sortBy') || 'startDate-asc'
+  const [field, direction] = sortBy.split('-')
+  const modifier = direction === 'asc' ? 1 : -1
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  )
 
   return (
     <Menus>
@@ -34,7 +40,7 @@ function CabinTable() {
           <div></div>
         </Table.Header>
         <Table.Body
-          data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
