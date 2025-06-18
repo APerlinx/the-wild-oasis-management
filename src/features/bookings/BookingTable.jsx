35326f6b2dac1356575e16149fa8394e1a52df97
APerlinx <133282938+APerlinx@users.noreply.github.com>
2025-06-18 12:57:01 +0300

    Add Sorting to bookings

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -19,6 +19,12 @@ function BookingTable() {
     if (currentFilter === 'all') return bookings
     return booking.status === currentFilter
   })
+  const sortBy = searchParams.get('sortBy') || 'startDate-asc'
+  const [field, direction] = sortBy.split('-')
+  const modifier = direction === 'asc' ? 1 : -1
+  const sortedBookings = filteredBookings.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  )
 
   return (
     <Menus>
@@ -33,7 +39,7 @@ function BookingTable() {
         </Table.Header>
 
         <Table.Body
-          data={filteredBookings}
+          data={sortedBookings}
           render={(booking) => (
             <BookingRow key={booking.id} booking={booking} />
           )}
