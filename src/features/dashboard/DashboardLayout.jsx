902f1f8b72086a9fadc15b07f76b742dff2806bb
APerlinx <133282938+APerlinx@users.noreply.github.com>
2025-06-25 18:26:26 +0300

    Add Pie chart

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -7,6 +7,7 @@ import Stats from './Stats'
 
 import Spinner from '../../ui/Spinner'
 import SalesChart from './SalesChart'
+import DurationChart from './DurationChart'
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -34,6 +35,7 @@ function DashboardLayout() {
         numDays={numDays}
         cabinCount={cabins.length}
       />
+      <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   )
