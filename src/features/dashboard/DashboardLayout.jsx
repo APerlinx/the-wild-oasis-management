491a67039b5db2e3e15c7c3196b6ef9c62c2a3b3
APerlinx <133282938+APerlinx@users.noreply.github.com>
2025-06-25 16:38:49 +0300

    Add dashboard layout and Recent Hooks

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,8 +1,26 @@
-import styled from "styled-components";
-
+import styled from 'styled-components'
+import { useRecentBookings } from './useRecentBookings'
+import Spinner from '../../ui/Spinner'
+import { useRecentStays } from './useRecentStays'
 const StyledDashboardLayout = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
-`;
+`
+
+function DashboardLayout() {
+  const { isLoading: isLoadingBookings, bookings } = useRecentBookings()
+  const { isLoading: isLoadingStays, stays, confirmedStays } = useRecentStays()
+  if (isLoadingBookings || isLoadingStays) return <Spinner />
+  return (
+    <StyledDashboardLayout>
+      <div>Statistic</div>
+      <div>Today{"'"}s activity</div>
+      <div>Chart 1</div>
+      <div>Chart 2</div>
+    </StyledDashboardLayout>
+  )
+}
+
+export default DashboardLayout
