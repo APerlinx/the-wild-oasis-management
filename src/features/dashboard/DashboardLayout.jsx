0c6c292143e7a0ba948044fd0ee178b9933b65a6
APerlinx <133282938+APerlinx@users.noreply.github.com>
2025-06-25 18:03:13 +0300

    Add Line chart to dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,7 +1,13 @@
+/* eslint-disable no-unused-vars */
 import styled from 'styled-components'
 import { useRecentBookings } from './useRecentBookings'
-import Spinner from '../../ui/Spinner'
 import { useRecentStays } from './useRecentStays'
+import { useCabins } from '../cabins/useCabins'
+import Stats from './Stats'
+
+import Spinner from '../../ui/Spinner'
+import SalesChart from './SalesChart'
+
 const StyledDashboardLayout = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
@@ -11,14 +17,24 @@ const StyledDashboardLayout = styled.div`
 
 function DashboardLayout() {
   const { isLoading: isLoadingBookings, bookings } = useRecentBookings()
-  const { isLoading: isLoadingStays, stays, confirmedStays } = useRecentStays()
-  if (isLoadingBookings || isLoadingStays) return <Spinner />
+  const {
+    isLoading: isLoadingStays,
+    stays,
+    confirmedStays,
+    numDays,
+  } = useRecentStays()
+  const { cabins, isLoading: isLoadingCabins } = useCabins()
+
+  if (isLoadingBookings || isLoadingStays || isLoadingCabins) return <Spinner />
   return (
     <StyledDashboardLayout>
-      <div>Statistic</div>
-      <div>Today{"'"}s activity</div>
-      <div>Chart 1</div>
-      <div>Chart 2</div>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins.length}
+      />
+      <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   )
 }
