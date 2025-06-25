904f02480e9baeabaea9106b915a841bdf6b9f8e
APerlinx <133282938+APerlinx@users.noreply.github.com>
2025-06-25 19:03:34 +0300

    Add Today Activity to dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -8,6 +8,7 @@ import Stats from './Stats'
 import Spinner from '../../ui/Spinner'
 import SalesChart from './SalesChart'
 import DurationChart from './DurationChart'
+import TodayActivity from '../check-in-out/TodayActivity'
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -20,7 +21,6 @@ function DashboardLayout() {
   const { isLoading: isLoadingBookings, bookings } = useRecentBookings()
   const {
     isLoading: isLoadingStays,
-    stays,
     confirmedStays,
     numDays,
   } = useRecentStays()
@@ -35,6 +35,7 @@ function DashboardLayout() {
         numDays={numDays}
         cabinCount={cabins.length}
       />
+      <TodayActivity />
       <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
